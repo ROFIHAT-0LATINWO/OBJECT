fdf929b4e98e74f3f5675e48eaf79d28e0c36be0
ROFIHAT-0LATINWO <rofiatolatinwo@gmail.com>
2022-09-27 04:32:33 -0700

    self introduction using java object

---
+++ self.js
@@ -0,0 +1,14 @@
+const self={
+name: "rofihat",
+age: 27,
+gender: "female",
+status: "single",
+complexion: "brown",
+skinType: "dry&sensitive",
+palmType: "water",
+birthLocation:"offa",
+almaMata:"kwara state university",
+lastPlaceVisited:"ibadan",
+presentPlaceVisited:"one innovation hub"
+}
+console.log(`my name is ${self.name}, aged ${self.age} years old, am a ${self.gender} and ${self.status} with a ${self.complexion} skin and a ${self.palmType}palm type, am birthed in ${self.birthLocation}, where i studied in ${self.almaMata}, i made my last visit to ${self.lastPlaceVisited}, am learning webdevelopment at ${self.presentPlaceVisited}`)
